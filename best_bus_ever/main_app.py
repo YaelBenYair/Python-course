d8d5a3e5a5909565c6080f3d6fc5d35abe0accc0
YaelBenYair <mayael49@telem.openu.ac.il>
2022-12-09 01:13:19 +0200

    uml class jpg and flow of the main_app

---
+++ best_bus_ever/main_app.py
@@ -0,0 +1,77 @@
+import os
+import pickle
+
+
+
+if __name__ == '__main__':
+
+    # check whether this is the first time you run the app
+    # if this is the first time - create a new class
+    if not os.path.exists('bus_company.pickle'):
+        pass
+        # bus_company = BestBusCompany()
+    else:
+        # this is not the first time - we already have a DB
+        # with data from the previous runs
+        with open('bus_company.pickle', 'rb') as fh:
+            pass
+            # bus_company = pickle.load(fh)
+
+    # input manager or passenger
+    # if manager or passenger
+    # manager:
+    # while < 3 - password = RideWithUs!
+    # Manager(bus_company)
+    # exit = False
+    # while not exit
+    # input(menu)
+    # match menu:
+    # case 1:
+    #     add_route()
+    # case 2:
+    #     delete_route()
+    # case 3:
+    #     update_route()
+    # case  4:
+    #     add_scheduled_ride()
+    # case 5:
+    #     exit = True
+    # loop until exit
+
+    # passenger:
+    # Passenger(bus_company)
+    # exit = False
+    # while not exit
+    # input(menu)
+    # match menu:
+    # case 1:
+    #     search_route
+    # case 2:
+    #     report_delay
+    # case 3:
+    #     exit = True
+    # loop until exit
+
+        # before exiting the program, persist the current state
+        # of te system in the file, so next time it will be loaded
+    with open('bus_company.pickle', 'wb') as fh:
+        pass
+        # pickle.dump(bus_company, fh)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
