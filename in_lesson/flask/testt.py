41310f5a740a80e4cf70374cf3f0cdb705a67efd
YaelBenYair <mayael49@telem.openu.ac.il>
2023-02-23 20:32:09 +0200

    flight react exercise

--- in_lesson/flask/testt.py
+++ in_lesson/flask/testt.py
@@ -1,7 +1,21 @@
-import re
-t = ['%s ilike %s', '%s ilike %s']
-m = ['name', '%yael%', 'address', '%tel%']
-sql = f"SELECT * FROM customer WHERE {' and '.join(t)};" % tuple(m)
-print(sql)
+# import re
+# t = ['%s ilike %s', '%s ilike %s']
+# m = ['name', '%yael%', 'address', '%tel%']
+# sql = f"SELECT * FROM customer WHERE {' and '.join(t)};" % tuple(m)
+# print(sql)
+#
+# print(re.match('[0-9]+$', '15342a'))
 
-print(re.match('[0-9]+$', '15342a'))
+value = 5
+# def func():
+#     value = 15
+#     print(value, end=" ")
+# func()
+# print(value)
+#
+# a = 1
+# b = 2
+# c = "3"
+#
+# print(a + b + c)
+
